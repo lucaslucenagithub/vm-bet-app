4ca4b075454559e2fc980bb42712e35f06633ed5
unknown <lucas.lucena@oantagonista.com>
2023-04-13 23:39:16 -0300

    next web app initial commit

---
+++ web/src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+
+function Navbar() {
+
+
+
+    return (
+        <div>
+            <Image src="/coin.svg" width={100} height={100} alt="coin" />
+        </div>
+    )
+}
+
+export default Navbar
+
